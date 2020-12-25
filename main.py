3a0cd98f94077b0380b3f9540af2ec8525865503
smazydev <alizafar1437@gmail.com>
2020-12-25 18:42:33 +0500

    implemented clear method that brings turtle to center

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ screen = Screen()
 def move_forwards():
     harry.forward(10)
 
+def move_backwards():
+    harry.forward(10)
+
+def move_counter_clockwise():
+    new_heading = harry.heading() + 10
+    harry.setheading(new_heading)
+
+def move_clockwise():
+    new_heading = harry.heading() - 10
+    harry.setheading(new_heading)
+
+def clear():
+    harry.clear()
+    harry.penup()
+    harry.home()
+    harry.pendown()
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counter_clockwise)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
