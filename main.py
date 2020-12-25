7c00f405cd5eddaa32174758ff485406efcee575
smazydev <alizafar1437@gmail.com>
2020-12-25 14:22:52 +0500

    implemented turtle moving on keystroke

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello worldS")
+from turtle import Turtle, Screen
+
+harry = Turtle()
+screen = Screen()
+
+def move_forwards():
+    harry.forward(10)
+
+screen.listen()
+screen.onkey(key="space", fun=move_forwards)
+screen.exitonclick()
